6e21503d1f9f394eb09047a9573495599abc5b70
hienpham15 <phamxuanhien15@gmail.com>
2021-07-18 05:54:03 +0200

    AC:prefix sum and pointers to tracck lowest values

--- Codeforces_round733/C.py
+++ Codeforces_round733/C.py
@@ -15,31 +15,55 @@ parse_input = lambda: sys.stdin.readline().rstrip("\r\n")
 def func(n_stages, my_points, il_points):
     selected = n_stages - n_stages//4
     
-    my_points.sort(reverse=True)
-    il_points.sort(reverse=True)
+    my_points.sort()
+    il_points.sort()
     
-    my_highpoint = sum(my_points[:selected])
-    il_highpoint = sum(il_points[:selected])
+    my_points_prefix = [0]
+    il_points_prefix = [0]
+    for i in range(n_stages):
+        my_points_prefix.append(my_points_prefix[i] + my_points[i])
+        il_points_prefix.append(il_points_prefix[i] + il_points[i])
+        
+    my_points_prefix = my_points_prefix[1:]
+    il_points_prefix = il_points_prefix[1:]
+        
+    if n_stages < 4:
+        my_highpoint = my_points_prefix[n_stages - 1]
+        il_highpoint = il_points_prefix[n_stages - 1]
+    else:
+        my_highpoint = my_points_prefix[n_stages -1] - my_points_prefix[n_stages//4 - 1]
+        il_highpoint = il_points_prefix[n_stages -1] - il_points_prefix[n_stages//4 - 1]
+    
+    my_k = n_stages//4
+    il_k = n_stages//4
+    my_lowest = my_points[my_k]
     
     count = 0
     while my_highpoint < il_highpoint:
-        my_points.insert(0, 100)
-        il_points.append(0)
+      
         n_stages += 1
         
-        selected = n_stages - n_stages//4
-        my_highpoint = sum(my_points[:selected])
-        il_highpoint = sum(il_points[:selected])
-        
+        if n_stages%4 != 0:
+            my_highpoint += 100
+            
+            if il_k - 1 >= 0:
+                il_highpoint += il_points[il_k - 1]
+                il_k -= 1
+                
+        else:
+            my_highpoint += 100 - my_lowest 
+            my_k += 1
+            my_lowest = my_points[my_k]
+            
         count +=1
         
     return count
 
 
-#n_stages = 7
-#my = [7, 59, 62, 52, 27, 31, 55]
-#il = [33, 35, 50, 98, 83, 80, 64]
-#ans = func(n_stages, my, il)
+n_stages = 4
+my = [20, 30, 40, 50]
+il = [100, 100, 100, 100]
+ans = func(n_stages, my, il)
 
 def main():
     n_cases = int(parse_input())
--- Codeforces_round733/D.py
+++ Codeforces_round733/D.py
@@ -15,8 +15,6 @@ parse_input = lambda: sys.stdin.readline().rstrip("\r\n")
 def func(n, wishes):
     bag = {}
     
-    naughty = {}
-    
     for i, wish in enumerate(wishes):
         if wish in bag:
             bag[wish] += [i+1]
@@ -41,20 +39,21 @@ def func(n, wishes):
             bag[i+1] = [available.pop()]
     
     for key, values in bag.items():
+        
         assigned[values[0] - 1] = key
             
         
     return d_wishes, assigned
 
-n = 7
-wishes = [6, 4, 6, 2, 4, 5, 6]
+n = 3
+wishes = [2, 1, 2]
 ans = func(n, wishes)
 
 
 def main():
     n_cases = int(parse_input())
     for i in range(n_cases):
-        n = int(parse_input().split())
+        n = int(parse_input())
         wishes = [int(i) for i in parse_input().split()]
         d_wishes, assigned = func(n, wishes)
         print(d_wishes)
