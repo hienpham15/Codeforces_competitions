6ca55d9f21e3873a7cab6e6c8e8309f7933b2bc5
hienpham15 <phamxuanhien15@gmail.com>
2021-08-19 21:49:04 +0200

    Code from solution

--- Codeforces_round739/C.py
+++ Codeforces_round739/C.py
@@ -12,66 +12,22 @@ import sys
 
 parse_input = lambda: sys.stdin.readline().rstrip("\r\n")
 
-def is_in_table(prev_x, x):
-    if x+1 <= prev_x:
-        return True
+
+def function(k):
+    
+    a, x, i = 1, 1, 1
+    
+    while (k >= x + a):
+        x += a
+        a += 2
+        i += 1
+    m = k - x + 1
+    if m <= i:
+        return (m, i)
     else:
-        return False
+        return (i, i - (m - i))
     
 
-
-def function(k, d):
-    
-    if k in d.keys():
-        return d[k]
-    else:
-        ind = list(d.keys())[-1]
-        tour = True
-        left = False
-        #down = False
-        x, y = d[ind]
-        prev_x = 1
-        for i in range(ind+1, k+1):
-            #x, y = d[i-1]
-            
-            if tour:
-                y += 1
-                tour = False
-                y_next = y 
-                d[i] = (x, y)
-                continue
-            
-            
-            if left:
-                y -= 1
-                d[i] = (x, y)
-                if y == 1 and not tour:
-                    left = False
-                    prev_x = x
-                    x = 1
-                    y = y_next
-                    tour = True
-                continue
-            
-            if not is_in_table(prev_x, x) and y>1:
-                x += 1
-                d[i] = (x, y)
-                left = True
-                continue
-            else:
-                x += 1
-                d[i] = (x, y)
-                #down = True
-                continue
-            
-
-        return d[k]
-    
-
-k = 14
-d = {}
-d[1] = (1, 1)
-ans = function(k, d)
 
 def main():
     n_cases = int(parse_input())
