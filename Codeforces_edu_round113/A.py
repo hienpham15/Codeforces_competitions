53a373b3fd7f8b299be4e5d2cfe95e19d556c523
hienpham15 <phamxuanhien15@gmail.com>
2021-09-08 17:52:32 +0200

    TLE

--- Codeforces_edu_round113/A.py
+++ Codeforces_edu_round113/A.py
@@ -17,59 +17,16 @@ parse_input = lambda: sys.stdin.readline().rstrip("\r\n")
 
 
 def func(n, string):
-    l, r = 0, n - 1
     
-    while l < r:
-        sub = string[l:r+1]
-        c = Counter(sub)
-        if c['a'] == c['b']:
-            return l, r
-        elif c['a'] > c['b']:
-            if string[l] == 'a':
-                l += 1
-            elif string[r] == 'a':
-                r -= 1
-            else:
-                sub1 = string[l+1:r+1]
-                l1, r1 = func(len(sub1), sub1)
-                
-                if l1 == -1 and r1 == -1:
-                    sub2 = string[l:r]
-                    l2, r2 = func(len(sub2), sub2)
-                    if l2 == -1 and r2 == -1:
-                        return -1, -1
-                    else:
-                        l2_n = string.find(string[l2:r2+1])
-                        r2_n = l2_n + len(string[l2:r2+1]) - 1
-                        return l2_n, r2_n
-                else:
-                    l1_n = string.find(string[l1:r1+1])
-                    r1_n = l1_n + len(string[l1:r1+1]) - 1
-                    return l1_n, r1_n
-                    
-        elif c['a'] < c['b']:
-            if string[l] == 'b':
-                l += 1
-            elif string[r] == 'b':
-                r -= 1
-            else:
-                sub1 = string[l+1:r+1]
-                l1, r1 = func(len(sub1), sub1)
-                
-                if l1 == -1 and r1 == -1:
-                    sub2 = string[l:r]
-                    l2, r2 = func(len(sub2), sub2)
-                    if l2 == -1 and r2 == -1:
-                        return -1, -1
-                    else:
-                        l2_n = string.find(string[l2:r2+1])
-                        r2_n = l2_n + len(string[l2:r2+1]) - 1
-                        return l2_n, r2_n
-                else:
-                    l1_n = string.find(string[l1:r1+1])
-                    r1_n = l1_n + len(string[l1:r1+1]) - 1
-                    return l1_n, r1_n
+    if len(string) < 2:
+        return -1, -1
     
+    for l in range(n-1):
+        for r in range(l+1, n):
+            sub = string[l:r+1]
+            c = Counter(sub)
+            if c['a'] == c['b']:
+                return l, r
     return -1, -1
 
 #n = 1
